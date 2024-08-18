c232aed0e00b298d3921b110035bf13c3de442c2
Sarthakjanrao07 <sarthakjanrav@gmail.com>
2024-08-18 16:22:53 +0530

    Weather-App

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "2dae0a6d9f601aa2401b9e7e5a6ac144",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
